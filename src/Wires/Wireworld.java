fa4dd17f23cf18edca991a4b98f35df2c89135be
PAwel_Z <pawelzych97@gmail.com>
2017-05-12 21:09:13 +0200

    Initial

---
+++ src/Wires/Wireworld.java
@@ -0,0 +1,10 @@
+package Wires;
+
+/**
+ * Created by zychp_w10 on 12.05.2017.
+ */
+public class Wireworld {
+    public static void main (String[] args) {
+        System.out.println("IntelliJ Project Start");
+    }
+}
