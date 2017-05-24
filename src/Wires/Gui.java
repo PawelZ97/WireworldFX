e4629fb6351f3c642e4616eed0770623c62476f0
PAwel_Z <pawelzych97@gmail.com>
2017-05-25 00:35:49 +0200

    Added Tools menu, added drawtoolspanel for drawing

--- src/Wires/Gui.java
+++ src/Wires/Gui.java
@@ -17,14 +17,16 @@ public class Gui {
     private JPanel drawpanel;
     private JPanel controlpanel;
     private JPanel mainpanel;
-    private JButton electronTailButton;
-    private JButton electronHeadButton;
-    private JButton conductorButton;
     private JFormattedTextField generationsTextField1;
     private JButton stopButton;
     private JFormattedTextField delayTextField1;
     private JLabel infoLabel;
     private JButton stepButton;
+    private JButton electronTailButton;
+    private JButton conductorButton;
+    private JButton electronHeadButton;
+    private JButton emptyButton;
+    private JPanel drawtoolspanel;
 
     private Action saveAction;
     private Action saveAsAction;
@@ -75,33 +77,13 @@ public class Gui {
             runButton.setVerticalTextPosition(3);
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 10;
+            gbc.gridy = 7;
             gbc.fill = GridBagConstraints.HORIZONTAL;
             controlpanel.add(runButton, gbc);
-            electronTailButton = new JButton();
-            electronTailButton.setBackground(new Color(-14601039));
-            electronTailButton.setForeground(new Color(-1));
-            electronTailButton.setText("Electron tail");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 2;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            controlpanel.add(electronTailButton, gbc);
-            conductorButton = new JButton();
-            conductorButton.setBackground(new Color(-924610));
-            conductorButton.setEnabled(true);
-            conductorButton.setForeground(new Color(-1));
-            conductorButton.setHideActionText(false);
-            conductorButton.setText("Conductor");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            controlpanel.add(conductorButton, gbc);
             final JPanel spacer1 = new JPanel();
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 5;
+            gbc.gridy = 2;
             gbc.weighty = 5.0;
             gbc.fill = GridBagConstraints.VERTICAL;
             controlpanel.add(spacer1, gbc);
@@ -109,7 +91,7 @@ public class Gui {
             generationsTextField1.setText("");
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 9;
+            gbc.gridy = 6;
             gbc.anchor = GridBagConstraints.WEST;
             gbc.fill = GridBagConstraints.HORIZONTAL;
             controlpanel.add(generationsTextField1, gbc);
@@ -117,32 +99,23 @@ public class Gui {
             label1.setText("Generations:");
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 8;
+            gbc.gridy = 5;
             gbc.anchor = GridBagConstraints.WEST;
             controlpanel.add(label1, gbc);
-            electronHeadButton = new JButton();
-            electronHeadButton.setBackground(new Color(-4521972));
-            electronHeadButton.setForeground(new Color(-1));
-            electronHeadButton.setText("Electron head");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 1;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            controlpanel.add(electronHeadButton, gbc);
             stopButton = new JButton();
             stopButton.setBackground(new Color(-60928));
             stopButton.setForeground(new Color(-16777216));
             stopButton.setText("Stop");
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 12;
+            gbc.gridy = 9;
             gbc.fill = GridBagConstraints.HORIZONTAL;
             controlpanel.add(stopButton, gbc);
             delayTextField1 = new JFormattedTextField();
             delayTextField1.setText("");
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 7;
+            gbc.gridy = 4;
             gbc.anchor = GridBagConstraints.WEST;
             gbc.fill = GridBagConstraints.HORIZONTAL;
             controlpanel.add(delayTextField1, gbc);
@@ -150,14 +123,14 @@ public class Gui {
             label2.setText("Delay:");
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 6;
+            gbc.gridy = 3;
             gbc.anchor = GridBagConstraints.WEST;
             controlpanel.add(label2, gbc);
             final JLabel label3 = new JLabel();
             label3.setText("Generation no:");
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 3;
+            gbc.gridy = 0;
             gbc.anchor = GridBagConstraints.WEST;
             controlpanel.add(label3, gbc);
             infoLabel = new JLabel();
@@ -166,27 +139,94 @@ public class Gui {
             infoLabel.setText("gennum");
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 4;
+            gbc.gridy = 1;
             controlpanel.add(infoLabel, gbc);
             stepButton = new JButton();
-            stepButton.setBackground(new Color(-3080448));
+            stepButton.setBackground(new Color(-924610));
             stepButton.setForeground(new Color(-16777216));
             stepButton.setText("Step");
             gbc = new GridBagConstraints();
             gbc.gridx = 0;
-            gbc.gridy = 11;
+            gbc.gridy = 8;
             gbc.fill = GridBagConstraints.HORIZONTAL;
             controlpanel.add(stepButton, gbc);
             drawpanel = new JPanel();
             drawpanel.setLayout(new GridBagLayout());
             drawpanel.setMinimumSize(new Dimension(0, 0));
             gbc = new GridBagConstraints();
-            gbc.gridx = 1;
+            gbc.gridx = 2;
             gbc.gridy = 0;
             gbc.weightx = 20.0;
             gbc.weighty = 20.0;
             gbc.fill = GridBagConstraints.BOTH;
             mainpanel.add(drawpanel, gbc);
+            drawtoolspanel = new JPanel();
+            drawtoolspanel.setLayout(new GridBagLayout());
+            gbc = new GridBagConstraints();
+            gbc.gridx = 1;
+            gbc.gridy = 0;
+            gbc.fill = GridBagConstraints.BOTH;
+            mainpanel.add(drawtoolspanel, gbc);
+            conductorButton = new JButton();
+            conductorButton.setBackground(new Color(-924610));
+            conductorButton.setEnabled(true);
+            conductorButton.setForeground(new Color(-16777216));
+            conductorButton.setHideActionText(false);
+            conductorButton.setText("Conductor");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            drawtoolspanel.add(conductorButton, gbc);
+            electronHeadButton = new JButton();
+            electronHeadButton.setBackground(new Color(-4521972));
+            electronHeadButton.setForeground(new Color(-16777216));
+            electronHeadButton.setText("Electron head");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            drawtoolspanel.add(electronHeadButton, gbc);
+            electronTailButton = new JButton();
+            electronTailButton.setBackground(new Color(-14601039));
+            electronTailButton.setForeground(new Color(-16777216));
+            electronTailButton.setText("Electron tail");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 2;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            drawtoolspanel.add(electronTailButton, gbc);
+            emptyButton = new JButton();
+            emptyButton.setBackground(new Color(-8290180));
+            emptyButton.setForeground(new Color(-16777216));
+            emptyButton.setText("Empty");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 3;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            drawtoolspanel.add(emptyButton, gbc);
+            final JPanel spacer2 = new JPanel();
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 4;
+            gbc.weighty = 2.0;
+            gbc.fill = GridBagConstraints.VERTICAL;
+            drawtoolspanel.add(spacer2, gbc);
+            final JLabel label4 = new JLabel();
+            label4.setText("Components:");
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 5;
+            gbc.anchor = GridBagConstraints.WEST;
+            drawtoolspanel.add(label4, gbc);
+            final JScrollPane scrollPane1 = new JScrollPane();
+            scrollPane1.setVerticalScrollBarPolicy(22);
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 6;
+            gbc.weighty = 1.0;
+            gbc.fill = GridBagConstraints.BOTH;
+            drawtoolspanel.add(scrollPane1, gbc);
 
 
             drawboard.setActual(logic.getBefore());
@@ -252,6 +292,36 @@ public class Gui {
                 }
             });
             menuBar.add(fileMenu);
+            JMenu editMenu = new JMenu("Tools");
+            editMenu.add(new AbstractAction("Run")
+            {
+                public void actionPerformed(ActionEvent event)
+                {
+                   controlpanel.setVisible(true);
+                   drawtoolspanel.setVisible(false);
+                    frame.pack();
+                }
+            });
+            editMenu.add(new AbstractAction("Draw")
+            {
+                public void actionPerformed(ActionEvent event)
+                {
+                    drawtoolspanel.setVisible(true);
+                    controlpanel.setVisible(false);
+                    frame.pack();
+                }
+            });
+            editMenu.add(new AbstractAction("Booth")
+            {
+                public void actionPerformed(ActionEvent event)
+                {
+                    drawtoolspanel.setVisible(true);
+                    controlpanel.setVisible(true);
+                    frame.pack();
+                }
+            });
+            menuBar.add(editMenu);
+
 
             frame.add(mainpanel);
             frame.setJMenuBar(menuBar);
